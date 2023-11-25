6b213df02e4860ffce7a5daaab8664e661840ef7
ocsfwarch <tpartcal@gmail.com>
2023-11-25 09:41:28 -0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <span>Hello World</span>
+    </div>
+  );
+}
+
+export default App;
