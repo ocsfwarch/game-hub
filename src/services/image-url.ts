6dfa18bfb1c121caf0eaaf26e72534649a36525b
ocsfwarch <tpartcal@gmail.com>
2023-11-27 13:08:33 -0800

    Add game sorting

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
 
 const getCroppedImageUrl = (url: string) => {
+    if (!url) return ''
     const start = url.indexOf('/games')
     const newUrl = url.slice(0, start) + "/crop/600/400" + url.slice(start)
     return newUrl
