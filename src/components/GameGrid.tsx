a722874791bc181638be8569294b2e8eb9d0e5f3
ocsfwarch <tpartcal@gmail.com>
2023-11-27 11:16:15 -0800

    Initial Commit - platform selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ interface Props {
 const GameGrid = ({ selectedGenre }: Props) => {
   const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6];
-  console.log(`selectedGenre = ${selectedGenre}`);
+
   return (
     <>
       {error && <Text>{error}</Text>}
